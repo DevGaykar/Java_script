b59abbced6b308affedb36bc3cc114566fdc1b0e
DevGaykar <151054333+DevGaykar@users.noreply.github.com>
2023-11-17 12:52:43 +0000

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,10 @@
+const customerid = 23 //declaration of constant variable
+let customername = "Dev" 
+let customercity = "Mumbai";
+let customerstate 
+var cust = "dev" //its not suggest because var does not understand scope
+cust1 = 324  //Variables can also  be defined without keywords 
+
+console.log(cust1)
+customername = 'nij'
+console.table([customerid,customername,customerstate,customercity])
