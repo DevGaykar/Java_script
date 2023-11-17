a0f8c953e3c505304450ff12ead53835faed2cde
DevGaykar <151054333+DevGaykar@users.noreply.github.com>
2023-11-17 13:26:21 +0000

    Data-Types

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,14 @@
+let num = 23 //int
+let str = "abcd" //string
+let bool = true //boolean
+let degree = null //null ...data-type(object)
+let state // undefined
+// data-type "symbol": which is unique
+console.table( [num,str,bool,degree,state])
+
+console.log(typeof num)
+console.log(typeof str)
+console.log(typeof bool)
+console.log(typeof state)
+console.log(typeof null) 
+/*its shortcoming of Js because null does not have it's seprate data-type*/
