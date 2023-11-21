a9d24cb87d0462ee633cd36d83db50f2c2f98060
DevGaykar <151054333+DevGaykar@users.noreply.github.com>
2023-11-21 14:59:46 +0000

    Comparisons in java

---
+++ 01_Basics/05_Comparisions.js
@@ -0,0 +1,28 @@
+// console.log(1>2);
+// console.log(1<2);
+// console.log(1==2);
+// console.log(1>=2);
+// console.log(1<=2);
+// console.log(1!=2);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+// console.log("2sc" > 1);
+/*
+Its converts string into number and then compers it
+*/
+
+// console.log(null == 0);
+// console.log(null < 0);
+// console.log(null > 0);
+// console.log(null <= 0);
+// //Comparision converts null to number teating it as 0
+// console.log(null >= 0);
+
+//It willl always be false
+// console.log(undefined==0);
+// console.log(undefined<0);
+// console.log(undefined>0);
+
+// "===" -> Its  strict check it won't change the data-type for comparison
+console.log("2" === 2);
