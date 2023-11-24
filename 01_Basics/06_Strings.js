2604fd4542581f8056344570588bc4bb2c4af66e
DevGaykar <151054333+DevGaykar@users.noreply.github.com>
2023-11-24 14:52:11 +0000

    String Functions

---
+++ 01_Basics/06_Strings.js
@@ -0,0 +1,47 @@
+const cust_id = 233
+let cust_name = " Dev"
+// console.log(cust_id.length); //it will give undefined
+// console.log(cust_id + cust_name) //we dont use this one way
+// console.log(`Customer's name ${cust_id} and his id is ${cust_id}`);
+
+// ******** String Functions *******
+let new_str = new String("The cust id is given as above")
+// console.log(new_str.length)
+// console.log(new_str.split(" "))
+// console.log(new_str.replace("cust","customer"))
+// console.log(new_str.anchor("id"))
+
+atstr = new_str.at(7)
+// console.log(atstr);
+// console.log(new_str.big());
+// console.log(new_str.blink());
+// console.log(new_str.bold());
+// console.log(new_str.charAt(7));
+// console.log(new_str.charCodeAt(7));
+// console.log(new_str.codePointAt(7));
+// console.log(new_str.concat(" are"));
+// console.log(new_str.endsWith("e"));
+// console.log(new_str.fixed());
+// console.log(new_str.fontcolor("green"));
+// console.log(new_str.fontsize(32));
+// console.log(new_str.includes("if"));
+// console.log(new_str.indexOf("d"));
+// console.log(new_str.isWellFormed());
+// console.log(new_str.italics());
+// console.log(new_str.lastIndexOf("i"));
+// console.log(new_str.link("https://google.com/"));
+// console.log(new_str.localeCompare("a"));
+// console.log(new_str.repeat(3));
+// console.log(new_str.replace("cust","customer"));
+// console.log(new_str.replaceAll("cust","customer"));
+// console.log(new_str.search("i"));
+// console.log(new_str.slice(3,7));
+// console.log(new_str.split(" "));
+// console.log(new_str.startsWith("The c",4));
+// console.log(new_str.small());
+// console.log(new_str.toUpperCase());
+// console.log(new_str.toLowerCase());
+// let trim_sen = "  Hello    "
+// console.log(trim_sen.trim());
+// console.log(trim_sen.trimStart());
+// console.log(trim_sen.trimEnd());
