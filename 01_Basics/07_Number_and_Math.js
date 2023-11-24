2ce157b49eae601ddbe0d0f8ec426572f070ba9c
DevGaykar <151054333+DevGaykar@users.noreply.github.com>
2023-11-24 15:01:26 +0000

    Number and Maths Module

---
+++ 01_Basics/07_Number_and_Math.js
@@ -0,0 +1,22 @@
+const score = 34
+// console.log(score); 
+
+const num1 = new Number(1189.435)
+// console.log(num1)
+
+// console.log(num1.toExponential(1))
+// console.log(num1.toFixed(2))
+// console.log(num1.toPrecision(3));
+// console.log(num1.toString());
+// console.log(num1.toLocaleString());
+
+
+// console.log(Math.floor(num1))
+// console.log(Math.ceil(num1))
+// console.log(Math.round(num1))
+
+// console.log(Math.random());//Random number between 0-1
+// console.log((Math.random()*10)+1)//Random numbers greater than zero
+max = 6
+min = 1
+console.log(Math.floor(Math.random()*(max-min+1))+min)//Random number for specified value
